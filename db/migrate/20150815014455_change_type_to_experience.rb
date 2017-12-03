8af05824b11ff194de00d2b71cb0a605b4ac8104
Paxoth <maximiliano.perez@usach.cl>
2017-12-03 13:19:42 -0300

    Heroku changes

--- db/migrate/20150815014455_change_type_to_experience.rb
+++ db/migrate/20150815014455_change_type_to_experience.rb
@@ -4,7 +4,7 @@ class ChangeTypeToExperience < ActiveRecord::Migration
 		change_column :experiences, :weekly_hours, :text
 		change_column :experiences, :benefit, :text
 		change_column :experiences, :students_level, :text
-		change_column :experiences, :professor_degree, 'integer USING CAST(product_code AS integer)'
+		change_column :experiences, :professor_degree, 'integer USING CAST(professor_degree AS integer)'
 		remove_reference :experiences, :offering
 		remove_reference :experiences, :request
 		add_reference :experiences, :service
